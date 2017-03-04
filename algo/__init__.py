cd2978bb4568bb72e88c43196439d17fdd5574e9
Maria Sandrikova <mashasandrikova@gmail.com>
2017-03-04 09:06:42 +0100

    Initial algo commit

---
+++ algo/__init__.py
@@ -0,0 +1,8 @@
+#####
+# Author: Maria Sandrikova
+# Copyright 2017
+#####
+
+from .runner import Runner
+
+__all__ = ["runner"]
---
+++ algo/runner.py
@@ -0,0 +1,17 @@
+#####
+# Author: Maria Sandrikova
+# Copyright 2017
+#####
+
+from typing import List
+
+import os
+
+import numpy as np
+
+class Runner:
+
+    def __init__(self):
+        pass
+
+   
