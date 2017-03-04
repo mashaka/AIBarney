88b75999de19260deb172bbc22d90630011f4dc3
Mark Riabov <m.ryadom@gmail.com>
2017-03-04 16:31:58 +0300

    mark_server

--- server/project/app/methods.py
+++ server/project/app/methods.py
@@ -1,6 +1,20 @@
 from .models import *
 
+import facebook
+facebook.VALID_API_VERSIONS = ['2.8']
+
+def get_token(user):
+    return (user.social_auth.get(provider='facebook').
+            extra_data['access_token'])
+
+def get_connection(user):
+    return facebook.GraphAPI(access_token=get_token(user),
+            version='2.8')
 
 def prepare_user(user):
-    
-    pass
+    profile, created = Profile.objects.get_or_create(user=user)
+    if created:
+        profile.avatar_url = get_connection(user).get_object(
+                'me/picture', height=160)['url']
+        profile.save()
+
