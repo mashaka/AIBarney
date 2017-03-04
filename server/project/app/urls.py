f39a68e6882f0f19119b1dee2d8e61bbee59f3e6
Mark Riabov <m.ryadom@gmail.com>
2017-03-04 16:36:25 +0300

    mark_server

--- server/project/app/urls.py
+++ server/project/app/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<chat_id>[0-9]+)/messages/$', views.MessageList.as_view(), name='message_list'),
+    url(r'^chat/(?P<chat_id>[0-9]+)/messages/$', views.MessageList.as_view(), name='message_list'),
 ]
