b105f971ff07253c14d40300351fa7594d578bb0
Mark Riabov <m.ryadom@gmail.com>
2017-03-05 06:35:20 +0300

    mark_server

--- algo/__init__.py
+++ algo/__init__.py
@@ -3,12 +3,12 @@
 # Copyright 2017
 #####
 
-from .chat_room import ChatRoom
+from .chat_room import ChatRoom, sentiment_model
 from .category import Category
 from .intersection import Intersection
 from .content import Content
 from .tip import Tip
 from .tools import CategoryType, InputData, UpdateInfo, UpdateType
-from .sentiment_analysis import train, classify, load_model, sentiment_model
+from .sentiment_analysis import train, classify, load_model
 
 __all__ = ['sentiment_analysis', 'chat_room', 'category', 'intersection', 'content', 'tip']
